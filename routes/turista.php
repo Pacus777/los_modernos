4e460c9454edbbff826509434e31ca02e03867f1
Nazareth75 <padillanazareth75@gmail.com>
2026-05-13 22:36:28 -0400

    T-22 Registro de ruta publica para perfil de emprendedor

--- routes/turista.php
+++ routes/turista.php
@@ -3,5 +3,16 @@
 use App\Http\Controllers\Turista\EmprendedorPublicoController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Rutas públicas del turista
+|--------------------------------------------------------------------------
+|
+| Esta ruta no usa auth porque el turista entra desde un QR.
+| El QR del emprendedor apuntará a:
+| /emprendedor/{id}
+|
+*/
+
 Route::get('/emprendedor/{id}', [EmprendedorPublicoController::class, 'show'])
     ->name('turista.emprendedor.show');
